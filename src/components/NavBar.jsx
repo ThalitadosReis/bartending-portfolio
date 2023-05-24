d5e03585de844a8ed2ccfa4839a2c4958a176599
Thalita dos Reis <dosreistha@gmail.com>
2023-05-24 11:10:12 +0200

    navbar setup

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,97 @@
+import { BrowserRouter as Router } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { Navbar, Nav, Container } from "react-bootstrap";
+
+export default function NavBar() {
+  const [activeLink, setActiveLink] = useState("home");
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const onScroll = () => {
+      if (window.scrollY > 50) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    };
+
+    window.addEventListener("scroll", onScroll);
+
+    return () => window.removeEventListener("scroll", onScroll);
+  }, []);
+
+  const onUpdateActiveLink = (value) => {
+    setActiveLink(value);
+  };
+
+  return (
+    <Router>
+      <Navbar
+        expand="md"
+        className={scrolled ? "scrolled" : ""}
+        style={{ padding: "10px" }}
+      >
+        <Container>
+          <Navbar.Brand
+            href="#home"
+            className={
+              activeLink === "home" ? "active navbar-link" : "navbar-link"
+            }
+            onClick={() => onUpdateActiveLink("home")}
+          >
+            <span>Bartender Brain</span>
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav">
+            <span className="navbar-toggler-icon"></span>
+          </Navbar.Toggle>
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              <Nav.Link
+                href="#about"
+                className={
+                  activeLink === "about" ? "active navbar-link" : "navbar-link"
+                }
+                onClick={() => onUpdateActiveLink("about")}
+              >
+                About
+              </Nav.Link>
+              <Nav.Link
+                href="#services"
+                className={
+                  activeLink === "services"
+                    ? "active navbar-link"
+                    : "navbar-link"
+                }
+                onClick={() => onUpdateActiveLink("services")}
+              >
+                Services
+              </Nav.Link>
+              <Nav.Link
+                href="#cocktails"
+                className={
+                  activeLink === "cocktails"
+                    ? "active navbar-link"
+                    : "navbar-link"
+                }
+                onClick={() => onUpdateActiveLink("cocktails")}
+              >
+                Cocktails
+              </Nav.Link>
+              <Nav.Link
+                href="#contact"
+                className={
+                  activeLink === "contact"
+                    ? "active navbar-link"
+                    : "navbar-link"
+                }
+                onClick={() => onUpdateActiveLink("contact")}
+              >
+                Contact
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </Router>
+  );
+}
