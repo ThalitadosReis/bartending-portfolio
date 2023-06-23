659b10e851b97e3640ffc6d78834134602deafaa
Thalita dos Reis <dosreistha@gmail.com>
2023-06-23 16:49:30 +0200

    service description + image adj

--- src/components/Service.jsx
+++ src/components/Service.jsx
@@ -4,12 +4,13 @@ export default function Service() {
   return (
     <div className="container my-5" id="service">
       <div className=" d-flex flex-column flex-md-row gap-3">
-        <img
-          className="w-50"
-          src="https://images.pexels.com/photos/4279101/pexels-photo-4279101.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-          alt=""
-        />
-        <div className="pt-2">
+        <div className="w-md-100">
+          <img
+            src="https://images.pexels.com/photos/4279101/pexels-photo-4279101.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            alt=""
+          />
+        </div>
+        <div className="w-md-75">
           <h3>Workshop</h3>
           <p>
             Lorem Ipsum is simply dummy text of the printing and typesetting
@@ -19,18 +20,18 @@ export default function Service() {
             only five centuries, but also the leap into electronic typesetting,
             remaining essentially unchanged.
           </p>
-          <br />
 
           <Link to="/workshop">More info</Link>
         </div>
       </div>
       <div className=" d-flex flex-column flex-md-row gap-3 my-5" id="bartend">
-        <img
-          className="w-50"
-          src="https://images.pexels.com/photos/6004589/pexels-photo-6004589.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-          alt=""
-        />
-        <div className="pt-2">
+        <div className="w-md-100">
+          <img
+            src="https://images.pexels.com/photos/6004589/pexels-photo-6004589.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            alt=""
+          />
+        </div>
+        <div className="w-md-75">
           <h3>Bartending</h3>
           <p>
             Lorem Ipsum is simply dummy text of the printing and typesetting
@@ -40,7 +41,6 @@ export default function Service() {
             only five centuries, but also the leap into electronic typesetting,
             remaining essentially unchanged.
           </p>
-          <br />
           <Link to="/bartending">More info</Link>
         </div>
       </div>
