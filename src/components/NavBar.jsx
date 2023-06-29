31b47ff88695472087f24e5a557e02c2a8970e51
Thalita dos Reis <dosreistha@gmail.com>
2023-06-29 21:02:32 +0200

    footer and nav logo

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,16 +1,20 @@
 import { Link } from "react-router-dom";
 import { Navbar, Nav, Container } from "react-bootstrap";
+import logo from "../assets/logo.svg";
 
 export default function NavBar() {
   return (
     <Navbar expand="md">
       <Container>
-        <Link to="/">Bartender Brain</Link>
+        <Link to="/" className="logo-container">
+          <img src={logo} alt="" className="logo-image" />
+          <span className="logo-text">Bartender Brain</span>
+        </Link>
         <Navbar.Toggle aria-controls="basic-navbar-nav">
           <span className="navbar-toggler-icon"></span>
         </Navbar.Toggle>
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ms-auto gap-3">
+          <Nav className="ms-auto gap-4">
             <Link to="/workshop">Workshop</Link>
             <Link to="/events">Private Events</Link>
             <Link to="/about">About</Link>
